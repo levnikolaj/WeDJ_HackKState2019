57634632028a123466e45b79de430ca85884acef
Kaid Maurice <kaidm@ksu.edu>
2019-11-09 10:58:51 -0600

    -save text

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 function sendAlert() {
-	alert("It's working");
+	alert(text);
+}
+
+function saveText() {
+  var text = document.getElementById('textbox').value;
+  localStorage.setItem("textbox", text); // save the item
 }
